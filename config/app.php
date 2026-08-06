15bf6ea5517131449c90b03b9039521554b8f886
Cau <cau.g@dotcubed.io>
2026-08-06 20:18:03 -0300

    Metadata nas páginas

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => 'Lunar Base',
-    'version' => '1.6.2',
+    'version' => '1.7.0',
     'author' => 'Cau Guanabara',
     'author_link' => 'https://cauguanabara.com.br',
 
