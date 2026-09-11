f6f6b93bee0c83bbe17cae14e3dd57bfd48e5817
Cau <cau.g@dotcubed.io>
2026-09-11 14:41:45 -0300

    Busca em users, filters, hide_themes

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,7 @@ return [
     App\Providers\PluginServiceProvider::class,
     App\Providers\ThemeServiceProvider::class,
     App\Providers\AppServiceProvider::class,
+    App\Providers\FilterServiceProvider::class,
     App\Providers\RenderServiceProvider::class,
     App\Providers\MailConfigServiceProvider::class,
 ];
